8a8d2ee24b0e0604bc28aef48486c5bcdf709a30
Swatigavhane <swatigavhane1996@gmail.com>
2026-08-22 23:44:37 +0530

    refactor(editor): use reducer-based state management

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -1,27 +1,76 @@
-import { useMemo, useState } from 'react';
-import { createEmptyDocument, serializeDocument, toggleMark } from '../model';
+import { useMemo, useReducer } from 'react';
+import { createEmptyDocument, getActiveMarks, serializeDocument, toggleMark } from '../model';
 import type { Document, Mark } from '../model';
 import type { EditorSelection } from '../editor-core/selection';
 import { buildSelection } from '../utils/buildSelection';
 import { COMMON_MARKS } from './constants';
 
+type EditorState = {
+  documentModel: Document;
+  selection: EditorSelection;
+};
+
+type EditorAction =
+  | {
+      type: 'selection/set';
+      selection: EditorSelection;
+    }
+  | {
+      type: 'mark/toggle';
+      mark: Mark;
+    };
+
+const createInitialState = (): EditorState => ({
+  documentModel: createEmptyDocument(),
+  selection: buildSelection('block-1', 0, 'block-1', 0),
+});
+
+const editorReducer = (state: EditorState, action: EditorAction): EditorState => {
+  switch (action.type) {
+    case 'selection/set':
+      return {
+        ...state,
+        selection: action.selection,
+      };
+    case 'mark/toggle':
+      return {
+        ...state,
+        documentModel: toggleMark(state.documentModel, state.selection, action.mark),
+      };
+    default:
+      return state;
+  }
+};
+
 export const useEditor = () => {
-  const [documentModel, setDocumentModel] = useState<Document>(() => createEmptyDocument());
-  const [selection, setSelection] = useState<EditorSelection>(() =>
-    buildSelection('block-1', 0, 'block-1', 0),
+  const [state, dispatch] = useReducer(editorReducer, undefined, createInitialState);
+
+  const activeMarks = useMemo(
+    () =>
+      getActiveMarks(state.documentModel, state.selection).map((mark: Mark) =>
+        typeof mark === 'string' ? mark : mark.type,
+      ),
+    [state.documentModel, state.selection],
   );
 
-  const activeMarks = useMemo(() => [], []);
+  const serializedDocument = useMemo(
+    () => serializeDocument(state.documentModel),
+    [state.documentModel],
+  );
 
   const toggle = (mark: Mark) => {
-    setDocumentModel((currentDocument: Document) => toggleMark(currentDocument, selection, mark));
+    dispatch({ type: 'mark/toggle', mark });
+  };
+
+  const setSelection = (nextSelection: EditorSelection) => {
+    dispatch({ type: 'selection/set', selection: nextSelection });
   };
 
   return {
-    documentModel,
-    selection,
+    documentModel: state.documentModel,
+    selection: state.selection,
     activeMarks,
-    serializedDocument: serializeDocument(documentModel),
+    serializedDocument,
     setSelection,
     toggleBold: () => toggle(COMMON_MARKS.bold),
     toggleItalic: () => toggle(COMMON_MARKS.italic),
